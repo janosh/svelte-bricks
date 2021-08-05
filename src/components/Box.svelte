22a68bc4328bb75c363661cb8c106cdc36310830
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-08-05 19:08:24 +0100

    add Toggle.svelte to control Box.svelte slideFlip prop

--- src/components/Box.svelte
+++ src/components/Box.svelte
@@ -1,42 +1,57 @@
 <script lang="ts">
   export let index: number
+  export let slideFlip = false
+
+  let flipped = false
 
   const height = 100 + 200 * Math.random()
 
   const randColor = () => Math.random().toString(16).substr(-6)
-  const bg = `linear-gradient(45deg, #${randColor()}, #${randColor()})`
 
-  let flipped = false
+  const bg = `linear-gradient(45deg, #${randColor()}, #${randColor()})`
 </script>
 
 <div
-  style="min-height: {height}px; background: {bg};"
+  style="min-height: {height}px;"
   on:click={() => (flipped = !flipped)}
-  class:flipped>
-  <p>h<sub>{index}</sub> = {Math.floor(height)}px</p>
-  <p style="transform: rotateY(180deg);">{bg}</p>
+  class:flipped
+  class:slideFlip>
+  <p style="background: {bg};">
+    <span>h<sub>{index}</sub> = {Math.floor(height)}px</span>
+  </p>
+  <p style="transform: rotateY(-180deg); background: {bg};"><span>{bg}</span></p>
 </div>
 
 <style>
   div {
     display: grid;
-    place-content: center;
-    border-radius: 1ex;
     transition: 0.5s;
     cursor: pointer;
     transform-style: preserve-3d;
   }
-  div.flipped {
-    transform: rotateY(180deg);
+  div p {
+    display: grid;
+    place-items: center;
+    backface-visibility: hidden;
+    grid-area: 1/1;
+    border-radius: 1ex;
+    margin: 0;
   }
   div:hover {
     transform: scale(1.03);
   }
-  div:hover.flipped {
-    transform: rotateY(180deg) scale(1.03);
+  div.flipped {
+    transform: rotateY(-180deg);
   }
-  div p {
-    backface-visibility: hidden;
-    grid-area: 1/1;
+  div:hover.flipped {
+    transform: rotateY(-180deg) scale(1.03);
+  }
+  div.flipped.slideFlip {
+    transform-origin: center right;
+    transform: translateX(-100%) rotateY(-180deg);
+  }
+  div:hover.flipped.slideFlip {
+    transform-origin: center right;
+    transform: translateX(-100%) rotateY(-180deg) scale(1.03);
   }
 </style>
