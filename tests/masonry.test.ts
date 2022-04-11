ec06d77e56ff5cf7958aeb8b9367e2f2335e995d
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-04-11 09:01:26 +0100

    add test 'Masonry > attaches class props correctly'

--- tests/masonry.test.ts
+++ tests/masonry.test.ts
@@ -1,18 +1,31 @@
-import { expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import Masonry from '../src/lib'
 
 const n_items = 30
 const indices = [...Array(n_items).keys()]
 
-test(`renders items`, async () => {
-  const masonry = new Masonry({
-    target: document.body,
-    props: { items: indices },
+describe(`Masonry`, () => {
+  test(`renders items`, async () => {
+    const masonry = new Masonry({
+      target: document.body,
+      props: { items: indices },
+    })
+
+    expect(masonry).toBeTruthy()
+
+    const items = document.querySelectorAll(`.masonry > .col > div`)
+
+    expect(items.length).toBe(n_items)
   })
 
-  expect(masonry).toBeTruthy()
+  test(`attaches class props correctly`, async () => {
+    new Masonry({
+      target: document.body,
+      props: { items: indices, class: `foo`, columnClass: `bar` },
+    })
 
-  const items = document.querySelectorAll(`.masonry > .col > div`)
+    const items = document.querySelectorAll(`.masonry.foo > .col.bar > div`)
 
-  expect(items.length).toBe(n_items)
+    expect(items.length).toBe(n_items)
+  })
 })
