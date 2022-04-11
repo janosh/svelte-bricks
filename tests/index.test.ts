68fafc860dcc421d74a24e150ff8cd0ce5c74e6b
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-04-11 09:07:07 +0100

    Add class props for use with CSS frameworks like Tailwind (#9)
    * pre-commit config set default_install_hook_types: [pre-commit, commit-msg]
    
    * update deps
    
    * address sveltekit breaking changes
    
    * import global.css in index.svelte for HMR
    
    * add props class + columnClass
    
    * readme document new CSS class props
    
    * add test 'Masonry > attaches class props correctly'
    
    * combine GH workflows test.yml and publish.yml into 1

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,6 +2,6 @@ import { expect, test } from 'vitest'
 import IndexMasonry from '../src/lib'
 import Masonry from '../src/lib/Masonry.svelte'
 
-test(`default export from index.ts is same as component file`, () => {
+test(`index.ts default export is component`, () => {
   expect(IndexMasonry).toBe(Masonry)
 })
