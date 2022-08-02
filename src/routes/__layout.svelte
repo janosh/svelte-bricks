a5790c2a6c7da7621793b1dfb456dd3768ffca0d
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-08-02 11:24:01 -0700

    add Masonry example rendering dynamically loaded images (closes #5)

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import GitHubCorner from 'svelte-github-corner'
+  import '../app.css'
+  import Header from '../components/Header.svx'
+</script>
+
+<GitHubCorner
+  href="https://github.com/janosh/svelte-bricks"
+  --ghc-color="var(--bodyBg)"
+  --ghc-bg="white"
+/>
+
+<Header />
+
+<slot />
+
+<style>
+  :global(h1) {
+    display: flex;
+    font-size: clamp(2rem, 2rem + 2vw, 3rem);
+    place-items: center;
+    place-content: center;
+    margin: 1.2em 0;
+  }
+  :global(h1 br) {
+    display: none;
+  }
+  @media (max-width: 600px) {
+    :global(h1) {
+      flex-direction: column;
+      gap: 1ex;
+      line-height: 1.1em;
+    }
+  }
+  :global(.hide-in-docs) {
+    display: none; /* needed to hide part of readme on deployed site */
+  }
+</style>
